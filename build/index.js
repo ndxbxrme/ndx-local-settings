f6e1a482a1dc289a9c10b7707c7b7145a94fe821
ndxbxrme <lewis_the_cat@hotmail.com>
2018-02-05 11:28:27 +0000

    use app name

--- build/index.js
+++ build/index.js
@@ -11,14 +11,15 @@
     module = angular.module('ndx', []);
   }
 
-  module.factory('LocalSettings', function(Auth) {
-    var getUserSettings, setUserSettings;
+  module.factory('LocalSettings', function(Auth, $rootElement) {
+    var appName, get, getGlobalSettings, getUserSettings, set, setGlobalSettings, setUserSettings;
+    appName = $rootElement.attr('ng-app');
     getUserSettings = function() {
       var output, user;
       output = {};
       if (localStorage && (user = Auth.getUser())) {
         try {
-          output = JSON.parse(localStorage.getItem(user._id)) || {};
+          output = JSON.parse(localStorage.getItem(`${appName}_${user._id}`)) || {};
         } catch (error) {}
       }
       return output;
@@ -26,38 +27,78 @@
     setUserSettings = function(value) {
       var user;
       if (localStorage && (user = Auth.getUser())) {
-        localStorage.setItem(user._id, JSON.stringify(value));
+        localStorage.setItem(`${appName}_${user._id}`, JSON.stringify(value));
       }
+    };
+    getGlobalSettings = function() {
+      var output;
+      output = {};
+      if (localStorage) {
+        try {
+          output = JSON.parse(localStorage.getItem(appName)) || {};
+        } catch (error) {}
+      }
+      return output;
+    };
+    setGlobalSettings = function(value) {
+      if (localStorage) {
+        localStorage.setItem(appName, JSON.stringify(value));
+      }
+    };
+    get = function(settings, key) {
+      var bit, bits, j, len, output;
+      bits = key.split(/\./g);
+      output = settings;
+      for (j = 0, len = bits.length; j < len; j++) {
+        bit = bits[j];
+        output = output[bit] || {};
+      }
+      if (JSON.stringify(output) === '{}') {
+        return null;
+      } else {
+        return output;
+      }
+    };
+    set = function(settings, key, value) {
+      var bit, bits, i, j, len, output, results;
+      bits = key.split(/\./g);
+      output = settings;
+      results = [];
+      for (i = j = 0, len = bits.length; j < len; i = ++j) {
+        bit = bits[i];
+        if (!output[bit]) {
+          output[bit] = {};
+        }
+        if (i < bits.length - 1) {
+          results.push(output = output[bit]);
+        } else {
+          results.push(output[bit] = value);
+        }
+      }
+      return results;
     };
     return {
       get: function(key) {
-        var bit, bits, j, len, output, settings;
+        var settings;
         settings = getUserSettings();
-        bits = key.split(/\./g);
-        output = settings;
-        for (j = 0, len = bits.length; j < len; j++) {
-          bit = bits[j];
-          output = output[bit] || {};
-        }
-        return output;
+        return get(settings, key);
       },
       set: function(key, value) {
-        var bit, bits, i, j, len, output, settings;
+        var settings;
         settings = getUserSettings();
-        bits = key.split(/\./g);
-        output = settings;
-        for (i = j = 0, len = bits.length; j < len; i = ++j) {
-          bit = bits[i];
-          if (!output[bit]) {
-            output[bit] = {};
-          }
-          if (i < bits.length - 1) {
-            output = output[bit];
-          } else {
-            output[bit] = value;
-          }
-        }
+        set(settings, key, value);
         return setUserSettings(settings);
+      },
+      getGlobal: function(key) {
+        var settings;
+        settings = getGlobalSettings();
+        return get(settings, key);
+      },
+      setGlobal: function(key, value) {
+        var settings;
+        settings = getGlobalSettings();
+        set(settings, key, value);
+        return setGlobalSettings(settings);
       }
     };
   });
--- src/index.coffee
+++ src/index.coffee
@@ -4,26 +4,35 @@ try
   module = angular.module 'ndx'
 catch e
   module = angular.module 'ndx', []
-module.factory 'LocalSettings', (Auth) ->
+module.factory 'LocalSettings', (Auth, $rootElement) ->
+  appName = $rootElement.attr 'ng-app'
   getUserSettings = ->
     output = {}
     if localStorage and user = Auth.getUser()
       try
-        output = JSON.parse(localStorage.getItem(user._id)) or {}
+        output = JSON.parse(localStorage.getItem("#{appName}_#{user._id}")) or {}
     output
   setUserSettings = (value) ->
     if localStorage and user = Auth.getUser()
-      localStorage.setItem user._id, JSON.stringify(value)
+      localStorage.setItem "#{appName}_#{user._id}", JSON.stringify(value)
     return
-  get: (key) ->
-    settings = getUserSettings()
+  getGlobalSettings = ->
+    output = {}
+    if localStorage
+      try
+        output = JSON.parse(localStorage.getItem(appName)) or {}
+    output
+  setGlobalSettings = (value) ->
+    if localStorage
+      localStorage.setItem appName, JSON.stringify(value)
+    return
+  get = (settings, key) ->
     bits = key.split /\./g
     output = settings
     for bit in bits
       output = output[bit] or {}
-    output
-  set: (key, value) ->
-    settings = getUserSettings()
+    if JSON.stringify(output) is '{}' then null else output
+  set = (settings, key, value) ->
     bits = key.split /\./g
     output = settings
     for bit, i in bits
@@ -33,4 +42,17 @@ module.factory 'LocalSettings', (Auth) ->
         output = output[bit]
       else
         output[bit] = value
+  get: (key) ->
+    settings = getUserSettings()
+    get settings, key
+  set: (key, value) ->
+    settings = getUserSettings()
+    set settings, key, value
     setUserSettings settings
+  getGlobal: (key) ->
+    settings = getGlobalSettings()
+    get settings, key
+  setGlobal: (key, value) ->
+    settings = getGlobalSettings()
+    set settings, key, value
+    setGlobalSettings settings
